d5c539a1961efc8970d9efbd5525786148b706f2
François Rullière <kapochamo@gmail.com>
2024-07-08 12:50:10 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -9,5 +9,5 @@ echo "\n🚀 Send to server"
 rsync -ravzh --filter=':- .gitignore'  . $USER@$SERVER:/home/$USER/Documents/$PROJECT/
 
 echo "\n🏃🏻‍♂️ Restart nginx" 
-sudo ln -s /home/$USER/Documents/$PROJECT/nginx.conf /etc/nginx/conf.d/$PROJECT.conf # Make sure the symlink exists
+ssh $USER@$SERVER "sudo ln -sf /home/$USER/Documents/$PROJECT/nginx.conf /etc/nginx/conf.d/$PROJECT.conf" # Make sure the symlink exists
 ssh $USER@$SERVER "sudo nginx -t && sudo nginx -s reload"
