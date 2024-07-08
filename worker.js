b1e35e11e5bc0f1dc3ae7395433af2118024de15
François Rullière <kapochamo@gmail.com>
2024-07-08 09:42:12 +0200

    pass 5

--- worker.js
+++ worker.js
@@ -4,80 +4,94 @@ const SIGNAL_RUN = 2;
 
 console.log("worker created");
 
-let simulate = null;
+let lastSetupEvent = null;
+
+const simulate = (event, sabViewPixels) => {
+  const {
+    sabViewParticles,
+    sabViewSimData,
+    id,
+    particleOffsetStart,
+    particleOffsetEnd,
+    particleStride,
+  } = event.data;
+
+  const [delta, mx, my, isTouch, width, height] = [
+    sabViewSimData[0],
+    sabViewSimData[1],
+    sabViewSimData[2],
+    !!sabViewSimData[3],
+    sabViewSimData[4],
+    sabViewSimData[5],
+  ];
+  const pixelChunkSize = width * height;
+  const pixelOffset = id * pixelChunkSize;
+  sabViewPixels.fill(0, pixelOffset, pixelOffset + pixelChunkSize);
+
+  const start = particleOffsetStart;
+  const end = particleOffsetEnd;
+  for (let i = start; i < end; i++) {
+    const decay = 1 / (1 + delta * 1);
+
+    const pi = i * particleStride;
+    let x = sabViewParticles[pi];
+    let y = sabViewParticles[pi + 1];
+    let dx = sabViewParticles[pi + 2] * decay;
+    let dy = sabViewParticles[pi + 3] * decay;
+
+    if (isTouch) {
+      const tx = mx - x;
+      const ty = my - y;
+      const dist = Math.sqrt(tx * tx + ty * ty);
+      const dirX = tx / dist;
+      const dirY = ty / dist;
+      const force = 3 * Math.min(1200, 25830000 / (dist * dist));
+      dx += dirX * force * delta;
+      dy += dirY * force * delta;
+    }
+    x += dx * delta;
+    y += dy * delta;
+    sabViewParticles[pi] = x;
+    sabViewParticles[pi + 1] = y;
+    sabViewParticles[pi + 2] = dx;
+    sabViewParticles[pi + 3] = dy;
+
+    if (x < 0 || x >= width) continue;
+    if (y < 0 || y >= height) continue;
+    const pixelIndex = (y | 0) * width + (x | 0);
+    const rx = x / width;
+    const ry = y / height;
+    const col = sabViewPixels[pixelOffset + pixelIndex];
+    const r = (col >> 16) & 0xff;
+    const g = (col >> 8) & 0xff;
+    const b = col & 0xff;
+    const fr = (clamp(r + 25 + 255 * rx * 0.2) & 0xff) << 16;
+    const fg = (clamp(g + 25 + 255 * ry * 0.2) & 0xff) << 8;
+    const fb = clamp(b + 25 + 255 * (1 - rx) * 0.2) & 0xff;
+    sabViewPixels[pixelOffset + pixelIndex] = fr | fg | fb;
+    // const r = sabViewPixels[pixelOffset + pixelIndex];
+    // const g = sabViewPixels[pixelOffset + pixelIndex + 1];
+    // const b = sabViewPixels[pixelOffset + pixelIndex + 2];
+    // sabViewPixels[pixelOffset + pixelIndex] = clamp(r + 25 + 35 * rx);
+    // sabViewPixels[pixelOffset + pixelIndex + 1] = clamp(g + 25 + 35 * ry);
+    // sabViewPixels[pixelOffset + pixelIndex + 2] = clamp(b + 25 + 35 * (1 - rx));
+  }
+
+  function clamp(n) {
+    n &= -(n >= 0);
+    return n | ((255 - n) >> 31);
+  }
+
+  postMessage({ id: SIGNAL_READY });
+};
 
 onmessage = (event) => {
-  if (!(event.data?.id >= 0) && simulate) {
-    simulate();
+  if (!(event.data?.id >= 0)) {
+    simulate(lastSetupEvent, event.data.sabViewPixels);
     return;
   }
 
-  const {
-    sabParticles,
-    sabSignals,
-    sabSimData,
-    sabPixelBuffs,
-    id,
-    chunkSize,
-    chunkOffset,
-    stride,
-  } = event.data;
+  lastSetupEvent = event;
 
-  const particlesView = new Float32Array(sabParticles);
-  const signalsView = new Int32Array(sabSignals);
-  const simDataView = new Float32Array(sabSimData);
-  const pixelBuffs = new Uint8ClampedArray(sabPixelBuffs);
-  const dt = () => simDataView[0];
-  const input = () => [
-    simDataView[1],
-    simDataView[2],
-    !!simDataView[3],
-    simDataView[4],
-    simDataView[5],
-  ];
-  console.log(`worker init ${id}`);
-  self.postMessage({ id: SIGNAL_READY });
-
-  simulate = () => {
-    const delta = dt();
-    const [mx, my, isTouch, width, height] = input();
-    const buffStride = width * height * 3;
-
-    pixelBuffs.fill(0, buffStride * id, buffStride * id + width * height * 3);
-    for (let i = chunkOffset; i < chunkOffset + chunkSize; i++) {
-      const decay = 1 / (1 + delta * 1);
-      let x = particlesView[i * stride];
-      let y = particlesView[i * stride + 1];
-      let dx = particlesView[i * stride + 2] * decay;
-      let dy = particlesView[i * stride + 3] * decay;
-
-      if (isTouch) {
-        const tx = mx - x;
-        const ty = my - y;
-        const dist = Math.sqrt(tx * tx + ty * ty);
-        const dirX = tx / dist;
-        const dirY = ty / dist;
-        const force = 3 * Math.min(1200, 25830000 / (dist * dist));
-        dx += dirX * force * delta;
-        dy += dirY * force * delta;
-      }
-      x += dx * delta;
-      y += dy * delta;
-      particlesView[i * stride] = x;
-      particlesView[i * stride + 1] = y;
-      particlesView[i * stride + 2] = dx;
-      particlesView[i * stride + 3] = dy;
-
-      if (x < 0 || x >= width) continue;
-      if (y < 0 || y >= height) continue;
-      const pixelIndex = ((y | 0) * width + (x | 0)) * 3;
-      const rx = x / width;
-      const ry = y / height;
-      pixelBuffs[buffStride * id + pixelIndex] += 25 + 50 * rx;
-      pixelBuffs[buffStride * id + pixelIndex + 1] += 25 + 50 * ry;
-      pixelBuffs[buffStride * id + pixelIndex + 2] += 25 + 50 * (1 - rx);
-    }
-
-    self.postMessage({ id: SIGNAL_READY });
-  };
+  postMessage({ id: SIGNAL_READY });
 };
